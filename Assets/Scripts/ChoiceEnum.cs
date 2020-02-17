62c284650c3b08150283f588d7492cab4d23d45e
Gustavo Santiago <egustavo4@gmail.com>
2020-02-16 23:48:33 -0300

    Create more example states

---
+++ Assets/Scripts/ChoiceEnum.cs
@@ -0,0 +1,5 @@
+﻿public enum ChoiceEnum
+{
+    OptionA,
+    OptionB,
+}
--- Assets/Scripts/State.cs
+++ Assets/Scripts/State.cs
@@ -3,12 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum ChoiceEnum
-{
-    OptionA,
-    OptionB
-}
-
 [CreateAssetMenu(menuName = "Dialog State")]
 public class State : ScriptableObject
 {
